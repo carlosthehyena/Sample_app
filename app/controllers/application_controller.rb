f87f16b4adb9b1b1ad00f7a9f7c56e77939540a7
wheresmyjumper <carlosm@fastmail.co.uk>
2015-06-06 15:42:01 +0000

    add hello message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def helloagain
+    render text: "Here we go again with Sample App"
+  end
+  
 end
