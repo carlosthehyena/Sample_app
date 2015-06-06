a7e894f2272adf40195cd0d9975fc72122789219
wheresmyjumper <carlosm@fastmail.co.uk>
2015-06-06 16:54:31 +0000

    update branch with latest

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,23 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
+ 
+  test "should get home2" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get help" do
+  test "should get help2" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get about2" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+  
 end
